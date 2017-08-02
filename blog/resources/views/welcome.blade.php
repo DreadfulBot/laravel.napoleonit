1736e131fdf1f1527bf822524b99ad7c0dd2646a
Artem <therawlex@gmail.com>
2017-08-02 10:22:37 +0500

    installed and enabled laravel-ide-helper, updated composer.json

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -76,7 +76,7 @@
                     @endauth
                 </div>
             @endif
-
+tes
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
